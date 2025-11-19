8579d034ffd364a6ce98644ea643e4e67ad77b9a
Sindre Gundersen <sindrebg@hotmail.com>
2025-11-19 15:50:35 +0100

    fix terraform1

--- oppgave1-terraform/providers.tf
+++ oppgave1-terraform/providers.tf
@@ -11,7 +11,11 @@ terraform {
 provider "aws" {
   region = var.aws_region
 
-  # In CI, avoid STS/account validation calls by toggling these flags
+  # In CI, avoid STS/account/IMDS calls by toggling these flags
   skip_credentials_validation = var.ci_mode
   skip_requesting_account_id  = var.ci_mode
+  skip_metadata_api_check     = var.ci_mode
+
+  # S3 path-style to minimize provider lookups in CI
+  s3_use_path_style           = var.ci_mode
 }
